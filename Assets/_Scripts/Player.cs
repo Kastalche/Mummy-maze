0e6ee857ad79bf71e81a25886503888afe9037ba
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-07-22 12:30:51 +0300

    Move the player movement in class PlayerMovement
    Move the movement from class Player into class PlayerMovement, so it can be easily reused for second player when developing multiplayer. Also now class Player looks more clean and clear.

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -11,13 +11,13 @@ using UnityEngine.Events;
 public class Player : MonoBehaviour
 {
     [SerializeField] private GridManager gridManager;
-
-    public UnityEvent PlayerMoved;
+    [SerializeField] private PlayerMovement playerMovement;
+    // public UnityEvent PlayerMoved;
     public Tile startPosition { get; private set; }
 
     void Awake()
     {
-        PlayerMoved = new UnityEvent();
+        //PlayerMoved = new UnityEvent();
     }
     private void Start()
     {
@@ -26,54 +26,10 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
-        Move();
+        playerMovement.GenerateMove();
     }
 
-    public void Move()
-    {
-        var playerPos = gridManager.tiles[(int)transform.position.x, (int)transform.position.y];
 
-        if (Input.GetKeyUp(KeyCode.D) && (int)transform.position.x + 1 < 6)
-        {
-            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x + 1, (int)playerPos.y], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-            {
-                transform.position = gridManager.tiles[(int)transform.position.x + 1, (int)transform.position.y].transform.position;
-                PlayerMoved?.Invoke();
-            }
-        }
-        else if (Input.GetKeyUp(KeyCode.A) && (int)transform.position.x - 1 > -1)
-        {
-            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x - 1, (int)playerPos.y], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-            {
-                transform.position = gridManager.tiles[(int)transform.position.x - 1, (int)transform.position.y].transform.position;
-                PlayerMoved?.Invoke();
-            }
-        }
-
-        else if (Input.GetKeyUp(KeyCode.W) && (int)transform.position.y + 1 < 6)
-        {
-            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x, (int)playerPos.y + 1], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-            {
-                transform.position = gridManager.tiles[(int)transform.position.x, (int)transform.position.y + 1].transform.position;
-                PlayerMoved?.Invoke();
-            }
-        }
-
-        else if (Input.GetKeyUp(KeyCode.S) && (int)transform.position.y - 1 > -1)
-        {
-            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x, (int)playerPos.y - 1], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-            {
-                transform.position = gridManager.tiles[(int)transform.position.x, (int)transform.position.y - 1].transform.position;
-                PlayerMoved?.Invoke();
-            }
-        }
-
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            PlayerMoved?.Invoke();
-        }
-
-    }
 
     public bool isAvailableFrom(Tile targetTile, Tile yourTile)
     {
--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,50 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private bool isMoving;
-    private Vector3 origPos, targetPos;
-    private float timeToMove = 0.2f;
+    [SerializeField] private GridManager gridManager;
+    [SerializeField] private Player player;
+    public UnityEvent PlayerMoved;
+    public Tile startPosition { get; private set; }
+    private void Start()
+    {
+        PlayerMoved = new UnityEvent();
+    }
 
     void Update()
     {
 
-        //if (Input.GetKey(KeyCode.W) && !isMoving)
-        //    StartCoroutine(MovePlayer(Vector3.up));
-
-        //if (Input.GetKey(KeyCode.A) && !isMoving)
-        //    StartCoroutine(MovePlayer(Vector3.left));
-
-        //if (Input.GetKey(KeyCode.S) && !isMoving)
-        //    StartCoroutine(MovePlayer(Vector3.down));
-
-        //if (Input.GetKey(KeyCode.D) && !isMoving)
-        //    StartCoroutine(MovePlayer(Vector3.right));
-
     }
-    //private IEnumerator MovePlayer(Vector3 direction)
-    //{
+    public void GenerateMove()
+    {
+        var playerPos = gridManager.tiles[(int)player.transform.position.x, (int)player.transform.position.y];
 
-    //    isMoving = true;
+        if (Input.GetKeyUp(KeyCode.D) && (int)player.transform.position.x + 1 < 6)
+        {
+            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x + 1, (int)playerPos.y], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
+            {
+                player.transform.position = gridManager.tiles[(int)player.transform.position.x + 1, (int)player.transform.position.y].transform.position;
+                PlayerMoved?.Invoke();
+                print("move");
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.A) && (int)player.transform.position.x - 1 > -1)
+        {
+            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x - 1, (int)playerPos.y], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
+            {
+                player.transform.position = gridManager.tiles[(int)player.transform.position.x - 1, (int)player.transform.position.y].transform.position;
+                PlayerMoved?.Invoke();
+                print("move");
+            }
+        }
 
-    //    float elapsedTime = 0;
+        else if (Input.GetKeyUp(KeyCode.W) && (int)player.transform.position.y + 1 < 6)
+        {
+            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x, (int)playerPos.y + 1], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
+            {
+                player.transform.position = gridManager.tiles[(int)player.transform.position.x, (int)player.transform.position.y + 1].transform.position;
+                PlayerMoved?.Invoke();
+                print("move");
+            }
+        }
 
-    //    origPos = transform.position;
-    //    targetPos = origPos + direction;
+        else if (Input.GetKeyUp(KeyCode.S) && (int)player.transform.position.y - 1 > -1)
+        {
+            if (isAvailableFrom(gridManager.tiles[(int)playerPos.x, (int)playerPos.y - 1], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
+            {
+                player.transform.position = gridManager.tiles[(int)player.transform.position.x, (int)player.transform.position.y - 1].transform.position;
+                PlayerMoved?.Invoke();
+                print("move");
+            }
+        }
 
-    //    while (elapsedTime < timeToMove)
-    //    {
-    //        transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
-    //        elapsedTime += Time.deltaTime;
-    //        yield return null;
-    //    }
-    //    transform.position = targetPos;
-    //    PlayerMoved?.Invoke();
-    //    isMoving = false;
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            PlayerMoved?.Invoke();
+            print("move");
+        }
+    }
+    public bool isAvailableFrom(Tile targetTile, Tile yourTile)
+    {
+        if (targetTile.obstacles.Count != 0)
+        {
+            if (targetTile.x != yourTile.x)   //if your x is diffrent
+            {
+                if (targetTile.x - yourTile.x == -1)    // u go left =>right
+                {
+                    if (targetTile.obstacles.Contains(3))
+                        return false;
+                    else return true;
+                }
+                else if (targetTile.x - yourTile.x == 1)   // righ => left
+                {
+                    if (targetTile.obstacles.Contains(1))
+                        return false;
+                    else return true;
+                }
+            }
+
+            if (targetTile.y != yourTile.y)
+            {
+                if (targetTile.y - yourTile.y == 1)
+                {
+                    if (targetTile.obstacles.Contains(4))   // up => down
+                        return false;
+                    else return true;
+                }
+                else if (targetTile.y - yourTile.y == -1)
+                {
+                    if (targetTile.obstacles.Contains(2))  //down => up
+                        return false;
+                    else return true;
+                }
+            }
+        }
+        return true;
+    }
 }
 
-// doTween
-//
-// event in player moved => game managera e subscribnat i kato go poluchi kazva na mummiqta da napravi dvijenie
