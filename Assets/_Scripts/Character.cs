7e5d69be3a453a1e60b0eb5f71f4a7511d0dd643
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-03 10:17:40 +0300

    Class character
    Character now merge Player and mummy classes.  Mummy and Player aren't diffrent things. Both are characters

---
+++ Assets/_Scripts/Character.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement; //i am not sure i need this.
+    public Tile startPosition { get; set; }
+    public bool isBot;
+    public bool isMummy;
+
+    public Tile TargetPosition;      //for mummy -> player from compare players, for players -> exit tile
+    public void GoToStartPosition()
+    {
+        transform.position = startPosition.transform.position;
+    }
+    //public int id;
+    //id ; isbot; input -> gridmng
+}
