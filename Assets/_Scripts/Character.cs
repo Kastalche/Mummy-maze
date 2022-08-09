c1f61a9abce0914118f657c9c971f429da62da17
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-09 11:39:43 +0300

    Cleaned Character.cs

--- Assets/_Scripts/Character.cs
+++ Assets/_Scripts/Character.cs
@@ -6,15 +6,16 @@ using UnityEngine.Events;
 public class Character : MonoBehaviour
 {
     [SerializeField] private CharacterMovement playerMovement; //i am not sure i need this.
+    private int id { get; set; }
+
     public Tile startPosition { get; set; }
+    public Tile TargetPosition;
+
     public bool isBot;
     public bool isMummy;
 
-    public Tile TargetPosition;      //for mummy -> player from compare players, for players -> exit tile
     public void GoToStartPosition()
     {
         transform.position = startPosition.transform.position;
     }
-    //public int id;
-    //id ; isbot; input -> gridmng
 }
