dcb5320d4ad6d66389dbf8628f5f41ddbf54ba04
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-09 11:21:02 +0300

    Update CharacterMovement
    Fixed CompareExplores and FindExploresTile

--- Assets/_Scripts/CharacterMovement.cs
+++ Assets/_Scripts/CharacterMovement.cs
@@ -7,6 +7,7 @@ using UnityEngine.Events;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private GridManager gridManager;
+    [SerializeField] private GameStateManagr gameManager;
 
     public UnityEvent ExplorerMoved;
     private void Start()
@@ -132,7 +133,7 @@ public class CharacterMovement : MonoBehaviour
     public void BotMoveHorizontally(Character mummy)
     {
         var mummyPos = mummy.transform.position;
-        var playerPos = FindTargetTile(mummy).transform.position;
+        var playerPos = FindExplorerTile(mummy).transform.position;
 
         if (mummyPos.x < playerPos.x)   //right
         {
@@ -179,28 +180,41 @@ public class CharacterMovement : MonoBehaviour
         mummy.transform.position = tile.transform.position;
     }
 
-    public Tile FindExplorerTile(Character mummy, Character player1, Character player2)
+    public Tile FindExplorerTile(Character mummy)
     {
-        var targetPlayer = CompareExplores(mummy, player1, player2);
-        mummy.TargetPosition.transform.position = targetPlayer.transform.position;
+        var targetPlayer = CompareExplores(mummy);
+        var target = targetPlayer.transform.position;
+        return gridManager.tiles[(int)target.x, (int)target.y];
         //if this row works I will buy myself a balkanche
     }
 
-
-    public Character CompareExplores(Character mummy, Character player1, Character player2)
+    public Character CompareExplores(Character mummy)
     {
-        if ((Math.Abs((int)mummy.transform.position.x - (int)player1.transform.position.x) > Math.Abs((int)mummy.transform.position.x - (int)player2.transform.position.x)))
-            return player2;
-        else if (player1.startPosition.x == player2.startPosition.x)
-            return player1;
-        else
-            return player1;
+        switch (gameManager.mode)
+        {
+            case GameModes.SinglePlayer:
+                return gameManager.characters[1];
+
+            case GameModes.Multiplayer:
+
+                var player1 = gameManager.characters[1];
+                var player2 = gameManager.characters[2];
+
+                if ((Math.Abs((int)mummy.transform.position.x - (int)player1.transform.position.x) > Math.Abs((int)mummy.transform.position.x - (int)player2.transform.position.x)))
+                    return player2;
+                else if (player1.startPosition.x == player2.startPosition.x)
+                    return player1;
+                else
+                    return player1;
+            default:
+                return gameManager.characters[1];
+        }
     }
 
     public bool MummyNotOnPlayersX(Character mummy)
     {
         var mummyPos = mummy.transform.position;
-        var playerPos = FindTargetTile().transform.position;
+        var playerPos = FindExplorerTile(mummy).transform.position;
 
         if (mummyPos.x != playerPos.x)
             return true;
@@ -209,61 +223,10 @@ public class CharacterMovement : MonoBehaviour
     public bool MummyNotOnPlayersY(Character mummy)
     {
         var mummyPos = mummy.transform.position;
-        var playerPos = FindTargetTile().transform.position;
+        var playerPos = FindExplorerTile(mummy).transform.position;
 
         if (mummyPos.y != playerPos.y)
             return true;
         else return false;
     }
-
 }
-
-
-
-
-
-
-
-//public void GenerateMoveSecondPlayer()
-//{
-//    var player = player2;
-//    var playerPos = gridManager.tiles[(int)player.transform.position.x, (int)player.transform.position.y];
-//    if (Input.GetKeyUp(KeyCode.D) && (int)player.transform.position.x + 1 < 6)
-//    {
-//        if (IsAvailableFrom(gridManager.tiles[(int)playerPos.x + 1, (int)playerPos.y], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-//        {
-//            player.transform.position = gridManager.tiles[(int)player.transform.position.x + 1, (int)player.transform.position.y].transform.position;
-//            SecondPlayersMoved?.Invoke();
-//        }
-//    }
-//    else if (Input.GetKeyUp(KeyCode.A) && (int)player.transform.position.x - 1 > -1)
-//    {
-//        if (IsAvailableFrom(gridManager.tiles[(int)playerPos.x - 1, (int)playerPos.y], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-//        {
-//            player.transform.position = gridManager.tiles[(int)player.transform.position.x - 1, (int)player.transform.position.y].transform.position;
-//            SecondPlayersMoved?.Invoke();
-//        }
-//    }
-
-//    else if (Input.GetKeyUp(KeyCode.W) && (int)player.transform.position.y + 1 < 6)
-//    {
-//        if (IsAvailableFrom(gridManager.tiles[(int)playerPos.x, (int)playerPos.y + 1], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-//        {
-//            player.transform.position = gridManager.tiles[(int)player.transform.position.x, (int)player.transform.position.y + 1].transform.position;
-//            SecondPlayersMoved?.Invoke();
-//        }
-//    }
-
-//    else if (Input.GetKeyUp(KeyCode.S) && (int)player.transform.position.y - 1 > -1)
-//    {
-//        if (IsAvailableFrom(gridManager.tiles[(int)playerPos.x, (int)playerPos.y - 1], gridManager.tiles[(int)playerPos.x, (int)playerPos.y]))
-//        {
-//            SecondPlayersMoved?.Invoke();
-//        }
-//    }
-
-//    else if (Input.GetKeyUp(KeyCode.Space))
-//    {
-//        SecondPlayersMoved?.Invoke();
-//    }
-//}
