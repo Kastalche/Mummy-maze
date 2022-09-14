14af211cbe187a106fa41b46657bbe5948bf8424
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-09-14 13:53:08 +0300

    Add method adding player ids to list with players

--- Assets/_Scripts/NetworkManager.cs
+++ Assets/_Scripts/NetworkManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class NetworkManager : MonoBehaviour
 {
     //important things here, take a list of players id from surves, remove all bot logics, and state machine logics, and when server send player id and players draw.
+    private List<string> playerIds = new List<string>();
     public static NetworkManager instance;
     public SocketIOComponent socket;
     void Awake()
@@ -18,7 +19,12 @@ public class NetworkManager : MonoBehaviour
     }
     void Start()
     {
+        socket.On("PlayerId", AddToPlayerList);
         socket.On("killExplorer", OnKilledExplorer);
+        socket.On("ApplyMove", OnApplyMove);
+        socket.On("ExploresTurn", OnExploresTurn);
+        socket.On("MummiesTurn", OnMummiesTurn);
+        socket.On("BatlleEnd", OnBattleEnd);
     }
 
     public void PlaySingle()
@@ -43,4 +49,32 @@ public class NetworkManager : MonoBehaviour
     {
         socket.Emit("KilledExplorer");
     }
+    public void OnExploresTurn(SocketIOEvent socketIOEvent)
+    {
+
+    }
+
+    public void OnMummiesTurn(SocketIOEvent socketIOEvent)
+    {
+
+    }
+
+    public void OnBattleEnd(SocketIOEvent socketIOEvent)
+    {
+
+    }
+
+    public void OnApplyMove(SocketIOEvent socketIOEvent)
+    {
+        string data = socketIOEvent.data.ToString();
+        Tile tile = JsonUtility.FromJson<Tile>(data);
+    }
+
+    public void AddToPlayerList(SocketIOEvent socketIOEvent)
+    {
+        string data = socketIOEvent.data.ToString();
+        playerIds.Add(data);
+
+    }
+
 }
